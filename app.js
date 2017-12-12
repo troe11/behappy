e76b74649aa05788bd32bdfeafb783b30b079e68
wyildd <vanssecody@gmail.com>
2017-12-11 18:27:58 -0700

    added firebase and js related links

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBpdGq5ONXfo854jcRj-4OwRWUYggFkRk4",
+    authDomain: "be-happy-web-appy.firebaseapp.com",
+    databaseURL: "https://be-happy-web-appy.firebaseio.com",
+    projectId: "be-happy-web-appy",
+    storageBucket: "be-happy-web-appy.appspot.com",
+    messagingSenderId: "232740001079"
+  };
+  firebase.initializeApp(config);
+  console.log(config)
 
+})
