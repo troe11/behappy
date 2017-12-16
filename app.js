d7f8f68b18a39ec4bbf9199711ee1e46e5f4f689
wyildd <vanssecody@gmail.com>
2017-12-16 10:26:33 -0700

    added more gif choices

--- app.js
+++ app.js
@@ -7,21 +7,41 @@
     messagingSenderId: "232740001079"
   };
   firebase.initializeApp(config);
-var signedOn = true;
 var database = firebase.database();
+
+var emotions = ['Happiness', 'Sadness', 'Disgust', 'Anger', 'Fear', 'Surprise', 'Neutral'];
+//the below function populates the Main Div with buttons corresponding to the emotions array
+var populateEmotions = function() {
+    var emoteButtonsDiv = $('<div>');
+    for (var i = 0; i < emotions.length; i++) {
+        var emotion = $('<button>').html(emotions[i]).attr('id', emotions[i]).addClass('emotion');
+        emoteButtonsDiv.append(emotion);
+    }
+    $('#main').empty();
+    $('#main').append(emoteButtonsDiv);
+}
+//runs when page loads
+populateEmotions();
+
+//when an emotion button is clicked, empty the Main Div, then use the choices function
+//to populate with new buttons - main choices: gif, reddit, etc.
+$(document).on('click', '.emotion', function() {
+    $('#main').empty();
+    choices();
+})
 
 var choices = function() {
     var choiceDiv = $('<div>');
-    var video = $('<button>').html('Video').attr('id', 'video');
-    var audio = $('<button>').html('Song').attr('id', 'audio');
+    var video = $('<button>').html('Video (n/a)').attr('id', 'video');
+    var audio = $('<button>').html('Song (n/a)').attr('id', 'audio');
     var reddit = $('<button>').html('Reddit').attr('id', 'reddit');
-    var pickUpLine = $('<button>').html('Pick Up Line').attr('id', 'pickUp');
+    var pickUpLine = $('<button>').html('Pick Up Line (n/a)').attr('id', 'pickUp');
     var giphy = $('<button>').html('Gif').attr('id', 'gif');
     choiceDiv.append(audio, video, reddit, pickUpLine, giphy);
     $('#main').append(choiceDiv);
 };
 
-$(document).on('click','#reddit', function() {
+$(document).on('click', '#reddit', function() {
     console.log('here')
     $('#main').empty();
     $.getJSON(
@@ -34,14 +54,11 @@ $(document).on('click','#reddit', function() {
                     if (post.data.title.includes("[Text]")) {
                         console.log(i, post.data.title);
                         var text = $('<div>').html(post.data.title.replace('[Text]', ''));
-                        $('#main').append(text)
-
-                    }
-                }
-            )
-        }
-    )
-
+                        $('#main').append(text)}
+                    
+                })
+                resartOrNewChoice();  
+        })
 })
 
 $(document).on('click','#gif', function() {
@@ -54,9 +71,9 @@ var types = function() {
     var cat = $('<button data-gif = "cat">').html('Cat').attr('class', 'giphy');
     var comedy = $('<button data-gif = "comedy">').html('Comedy').attr('class', 'giphy');
     var dog = $('<button data-gif = "dog">').html('Dog').attr('class', 'giphy');
-    // var  = $('<button>').html('').attr('class', 'giphy');
-    // var  = $('<button>').html('').attr('class', 'giphy');
-    typeDiv.append(cat, comedy, dog);
+    var random = $('<button data-gif = "random">').html('Random').attr('class', 'giphy');
+    var kawaii = $('<button data-gif = "kawaii">').html('Kawaii').attr('class', 'giphy');
+    typeDiv.append(cat, comedy, dog, random, kawaii);
     $('#main').append(typeDiv);
 };
 
@@ -73,16 +90,14 @@ $(document).on('click', '.giphy', function(){
     })
 })
 
-
-
 function displayGif(response) {
     $('#main').empty();
    var i = Math.floor((Math.random() * 60))
 
         
-        var image = '<img src= " ' + response.data[i].images.fixed_height_still.url +
+        var image = '<img src= " ' + response.data[i].images.fixed_height.url +
             '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:500px; height:500px">';
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="animate" class="movImage" style= "width:500px; height:500px">';
 
         image = '<div class = "center">' + image + "</div>";
         $('#main').append(image);
@@ -97,112 +112,27 @@ function displayGif(response) {
             $(this).attr('src', $(this).attr("data-still"));
             $(this).attr('data-state', 'still');
         }
-
     });
+    resartOrNewChoice();
 }
 
-$('.emotion').on('click', function() {
-    var user = firebase.auth().currentUser;
-    var currentEmote = $(this).attr('id');
-    var currentTime = moment().format('dddd HH:mm');
-    if (signedOn) {
-        console.log('Signed in');
-        $('#main').empty();
-        choices();
-        database.ref('/users').child(user.uid).push({
-            emotion: currentEmote,
-            datetime: currentTime
-        })
-    } else { alert('You need to sign In or Up') }
-    console.log(currentEmote, currentTime)
+//adds two buttons to the bottom, giving user the choice to go back to main
+//menu and choose new emotion, or select another choice
+var resartOrNewChoice = function() {
+    var newSelect = $('<div>').css('display','inline');
+    var newChoice = $('<button>').html('New Choice').attr('id', 'newChoice');
+    var resart = $('<button>').html('Change your Emotion?').attr('id', 'newEmotion');
+    newSelect.append(newChoice, resart);
+    $('#main').append(newSelect);
+}
 
-
+//runs choices if newChoice is clicked in resartOrNewChoice
+$(document).on('click', '#newChoice', function() {
+    $('#main').empty();
+    choices();
 })
-
-$('#signUp').on('click', function() {
-    event.preventDefault();
-    var email = $('#email').val();
-    var password = $('#password').val();
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
-
-    console.log(email, password)
-})
-
-$('#signIn').on('click', function() {
-    signedOn = true;
-    event.preventDefault();
-    var email = $('#email').val();
-    var password = $('#password').val();
-
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-    });
-
-    console.log(email, password)
-})
-
-$('#signOut').on('click', function() {
-
-
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-    }).catch(function(error) {
-        // An error happened.
-    });
-
-    console.log(email, password)
-})
-
-
-firebase.auth().onAuthStateChanged(function(user) {
-    console.log(user)
-    if (user) {
-        database.ref('/users').child(user.uid).once('value').then(function(snapshot) {
-            //Look up documentation for once method
-
-            $('#status').html('Signed In')
-
-            userName = snapshot.val().name;
-            userAge = snapshot.val().age;
-            $('#userName').text(userName);
-            $('#userAge').text(userAge);
-            if (user != null) {
-                user.providerData.forEach(function(profile) {
-                    console.log(profile)
-                    // console.log("Sign-in provider: " + profile.providerId);
-                    // console.log("  Provider-specific UID: " + profile.uid);
-                    // console.log("  Name: " + profile.displayName);
-                    // console.log("  Email: " + profile.email);
-                    // console.log("  Photo URL: " + profile.photoURL);
-                });
-
-            }
-        })
-    } else {
-        console.log('none')
-        $('#status').html('Signed Out')
-    }
-});
-
-$('#create').on('click', function() {
-    event.preventDefault();
-    var user = firebase.auth().currentUser;
-    if (user) {
-        console.log('Signed in');
-        database.ref('/users').child(user.uid).set({
-            name: $('#name').val().trim(),
-            age: $('#age').val().trim()
-        })
-    } else {
-        console.log('Not signed in')
-    }
+//runs original emotion div populator
+$(document).on('click', '#newEmotion', function() {
+    $('#main').empty();
+    populateEmotions();
 })
